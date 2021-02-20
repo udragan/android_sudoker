8a6801dc460b6b7ba55eb401d3b394ec3e6b984b
udragan <ujfa2001@yahoo.com>
2021-02-20 15:46:24 +0100

    added empty project

---
+++ solution/Sudoker/app/src/main/java/ns/fajnet/android/sudoker/MainActivity.kt
@@ -0,0 +1,13 @@
+
+
+package ns.fajnet.android.sudoker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
